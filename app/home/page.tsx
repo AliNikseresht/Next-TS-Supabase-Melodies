4f4239f32cef3b6fe8773162f2b637842c731840
Ali <alinksht@gmail.com>
2025-02-05 14:04:47 +0330

    add axios and add example data for test supabase

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+"use client";
 
-const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image_url: string;
 }
 
-export default HomePage
+const HomePage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("/api/products");
+
+        console.log("Fetched Data:", res.data);
+
+        if (res.data.error) throw new Error(res.data.error);
+        setProducts(res.data.data || []);
+      } catch (err) {
+        console.error("Error fetching products:", err);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <img src={product.image_url} alt={product.name} width="150" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
