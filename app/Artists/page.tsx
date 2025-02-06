35c43541a9a802fa2b66c695ec24cb4d4e4de300
Ali <alinksht@gmail.com>
2025-02-06 12:10:33 +0330

    change app folder

--- app/Artists/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ArtitsPage = () => {
-  return (
-    <div>ArtitsPage</div>
-  )
-}
-
-export default ArtitsPage
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <div className="loading loading-ring"></div>
+    </div>
+  );
+};
+
+export default loading;
